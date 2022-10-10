86a4ba5d35cf7040c8b583bc81ede58a50ab904a
plotnikov <plotnikov@rit-l4>
2022-10-10 16:54:07 +0300

    Refactoring.

--- twitter_user/views.py
+++ twitter_user/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from services.common_utils import urls_to_screen_name, too_many_request_handler
@@ -6,19 +7,15 @@ from services.twitter.utils import get_lists_of_twitter_users, get_last_tweets_b
 
 
 def get_index_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html')
 
 
 @too_many_request_handler
 def get_users_info(request):
     template = loader.get_template('table.html')
-    context = {}
     users_name = urls_to_screen_name(request.POST.getlist('urls'))
     users_data, errors = get_lists_of_twitter_users(users_name)
-    context['persons'] = users_data
-    context['errors'] = errors
+    context = {'persons': users_data, 'errors': errors}
     return HttpResponse(template.render(context, request))
 
 
@@ -26,6 +23,5 @@ def get_users_info(request):
 def get_user_tweets(request, person_id):
     template = loader.get_template('tweets.html')
     tweets = get_last_tweets_by_username(person_id)
-    context = {}
-    context['tweets'] = tweets
+    context = {'tweets': tweets}
     return HttpResponse(template.render(context, request))
